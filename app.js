cd365bc0417c5a224adfb5afd954db505785c21a
Simon Kuforiji <simonkuforiji@gmail.com>
2021-07-21 15:54:13 +0100

    added port option for heroku hosting

--- app.js
+++ app.js
@@ -61,7 +61,10 @@ app.get("/posts/:postTitle", function(req, res){
 
 })
 
-let port = 3000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 app.listen(port, function() {
   console.log("Server started on port 3000");
 });
